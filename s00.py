ff2b6fb7bb593f47d3e0bf3cf8eca9d029468a24
xkheb351aulg <imuuuuuuuuuuuuuuu+xkheb351aulg@gmail.com>
2024-12-19 00:12:01 +0800

    Update s00.py

--- s00.py
+++ s00.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from threading import Thread
 
-# 配置日志
+# 配置日
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # API 配置
