755f8030133d0bb7c20a4ea6048c80a771f90e62
xkheb351aulg <imuuuuuuuuuuuuuuu+xkheb351aulg@gmail.com>
2024-12-20 16:55:17 +0800

    Update s00.py

--- s00.py
+++ s00.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # API 配置 
-DATA_API_URL = "https://s00reg.64t76dee9sk5.workers.dev/api"
+DATA_API_URL = "https://shy-poetry-8661.ymyuuu.workers.dev/api"
 NOTIFICATION_API_URL = "https://api.day.app/Y6wZN8swvDrno2URYa5CDZ/"
 
 # 日志锁
